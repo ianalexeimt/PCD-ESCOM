18aa561f616a0fbebfe76b35942710ca9e1e2b12
ianalexeimt <ianalexeimt@gmail.com>
2026-06-23 15:19:00 -0600

    Función que valida empleados, envios y facturas

--- semana-06/main.py
+++ semana-06/main.py
@@ -17,3 +17,32 @@ def validar_producto (codigo):
         return True
     return False
 
+def validar_envio (codigo):
+    m=re.match(r'^ENV-(\d{4})-(\d{2})-(\d{2})-\d{6}$',codigo)
+    if m:
+        try:
+            anio=int(m.group(1))
+            mes=int(m.group(2))
+            dia=int(m.group(3))
+            if 2020 <= anio <= 2030 and 1 <= mes <= 12 and 1 <= dia <= 31:
+                return True
+        except ValueError:
+            pass
+    return False
+
+def validar_empleado (codigo):
+    m=re.match(r'^EMP-([A-Z]{3})-([1-9]\d{3})$',codigo)
+    if m:
+        departamento=m.group(1)
+        if departamento in ['VEN','ADM','TEC','LOG','RHH']:
+            return True
+    return False
+
+def validar_factura (codigo):
+    m=re.match(r'^FAC-([A-Z])-\d{6}$',codigo)
+    if m:
+        serie=m.group(1)
+        if serie in ['A','B','C','D','E']:
+            return True
+    return False
+
